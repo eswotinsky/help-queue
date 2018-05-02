0581cf4abd18ca835feb9aedd9d74ff85d1a099c
Eric <eric.swotinsky@gmail.com>
2018-05-02 15:31:45 -0700

    add Admin component and route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
+import Admin from './Admin';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import Moment from 'moment';
@@ -24,7 +25,6 @@ class App extends React.Component {
     }
 
     updateTicketElapsedWaitTime() {
-       console.log("check");
        let newTicketList = this.state.ticketList.slice();
        newTicketList.forEach((ticket) =>
          ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
@@ -50,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.state.ticketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl onNewTicket={this.handleNewTicket} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.ticketList} currentRouterPath={props.location.pathname} />} />
           <Route component={Error404} />
         </Switch>
       </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Ticket(props){
-  return (
-    <div>
-      <style jsx>{`
-          div {
-            background-color: ghostwhite;
-          }
-          h3 {
-            color: blue;
-          }
-        `}</style>
-      <h3>{props.location} - {props.names}</h3>
-      <h4>{props.formattedWaitTime} ago</h4>
-      <p><em>{props.issue}</em></p>
-      <hr/>
-    </div>
-  );
+  const ticketInformation =
+  <div>
+    <style jsx>{`
+        div {
+          background-color: ghostwhite;
+        }
+        h3 {
+          color: blue;
+        }
+      `}</style>
+    <h3>{props.location} - {props.names}</h3>
+    <h4>{props.formattedWaitTime} ago</h4>
+    <p><em>{props.issue}</em></p>
+    <hr/>
+  </div>;
+
+  if (props.currentRouterPath === '/admin'){
+    return (
+      <div onClick={() => {alert('hey you clicked the ticket belonging to ' + props.names);}}>
+        {ticketInformation}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {ticketInformation}
+      </div>
+    );
+  }
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-  formattedWaitTime: PropTypes.string.isRequired
+  formattedWaitTime: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string
 };
 
 export default Ticket;
